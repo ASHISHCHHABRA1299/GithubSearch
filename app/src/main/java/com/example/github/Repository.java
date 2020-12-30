779ea4ff63eac91cb08e1f6a375ff01fb8ca0113
hp <chhabraashish123@gmail.com>
2020-12-30 18:23:58 +0530

    Repository and adapter class created

---
+++ app/src/main/java/com/example/github/Repository.java
@@ -0,0 +1,13 @@
+package com.example.github;
+
+public class Repository {
+    String name;
+    String description;
+    String language;
+
+    public Repository(String name, String description, String language) {
+        this.name = name;
+        this.description = description;
+        this.language = language;
+    }
+}
