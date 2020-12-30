6f48a18fa9026c42190c898d67f080f6f8296360
hp <chhabraashish123@gmail.com>
2020-12-30 19:30:17 +0530

    Final Updated

--- app/src/main/java/com/example/github/PortfolioActivity.java
+++ app/src/main/java/com/example/github/PortfolioActivity.java
@@ -1,6 +1,9 @@
 package com.example.github;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -10,7 +13,10 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.bumptech.glide.Glide;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -21,6 +27,7 @@ public class PortfolioActivity extends AppCompatActivity {
     String user;
     ImageView profile;
     TextView follower,following,bio;
+    RecyclerView mRecyclerView;
     String url="https://api.github.com/users/";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +36,7 @@ public class PortfolioActivity extends AppCompatActivity {
         profile=(ImageView) findViewById(R.id.image);
         follower=(TextView)findViewById(R.id.followers);
         following=(TextView)findViewById(R.id.following);
+        mRecyclerView=(RecyclerView)findViewById(R.id.recyclerview);
         bio=(TextView)findViewById(R.id.bio);
         // to get the username from the mainactivity.java
         user=getIntent().getStringExtra("Username");
@@ -71,7 +79,27 @@ public class PortfolioActivity extends AppCompatActivity {
     // method to load the repositories
     public void loadrepository(String url)
     {
-
+        url=url+"/repos?per_page=100&sort=created";
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        GsonBuilder gsonBuilder=new GsonBuilder();
+                        Gson gson=gsonBuilder.create();
+                        Repository[] repoArray=gson.fromJson(response,Repository[].class);
+                        mRecyclerView.setLayoutManager(new LinearLayoutManager(PortfolioActivity.this));
+                        mRecyclerView.setHasFixedSize(true);
+                        RepositoryAdapter repositoryAdapter=new RepositoryAdapter(repoArray,PortfolioActivity.this);
+                        mRecyclerView.setAdapter(repositoryAdapter);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(PortfolioActivity.this, "Unable to fetch the data", Toast.LENGTH_SHORT).show();
+            }
+        });
+        MySingleton.getInstance(this).addToRequestQueue(stringRequest)  ;
     }
 
 
--- app/src/main/java/com/example/github/RepositoryAdapter.java
+++ app/src/main/java/com/example/github/RepositoryAdapter.java
@@ -1,4 +1,56 @@
 package com.example.github;
 
-public class RepositoryAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RepositoryAdapter extends RecyclerView.Adapter<RepositoryAdapter.RepoViewHolder> {
+
+    Repository[] repoarray;
+    Context mContext;
+
+    public RepositoryAdapter(Repository[] repoarray, Context context) {
+        this.repoarray = repoarray;
+        mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public RepoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater=LayoutInflater.from(parent.getContext());
+        View itemview=inflater.inflate(R.layout.repo_item,parent,false);
+        RepoViewHolder viewHolder=new RepoViewHolder(itemview);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RepoViewHolder holder, int position) {
+        Repository repo=repoarray[position];
+        holder.reponame.setText(repo.name);
+        holder.language.setText(repo.language);
+        holder.description.setText(repo.description);
+    }
+
+    @Override
+    public int getItemCount() {
+        return repoarray.length;
+    }
+
+    class RepoViewHolder extends RecyclerView.ViewHolder{
+
+        TextView reponame,description,language;
+        public RepoViewHolder(@NonNull View itemView) {
+            super(itemView);
+            reponame=(TextView)itemView.findViewById(R.id.name);
+            description=(TextView)itemView.findViewById(R.id.desc);
+            language=(TextView)itemView.findViewById(R.id.lang);
+        }
+    }
 }
